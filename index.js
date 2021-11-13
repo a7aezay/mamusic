b7c3f16d7606d93a18817a4a4d08a6bd3b371c54
amro <amr@Amrs-MacBook-Air.local>
2021-11-13 09:53:42 -0800

    copy start 1000'

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('a7a');
+#!/usr/bin/env -S UV_THREADPOOL_SIZE=1 NODE_OPTIONS='--experimental-modules' node
+
+const start = async function () {
+  const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+  await delay(7000) /// waiting 7 second.
+  console.log('a7a');
+}
+
+/* Copy */ start(); /* 1000 */
